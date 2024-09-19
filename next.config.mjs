c14450cb3b5a740a42993d1164819ccce4ebee4c
Menajul Hoque <menajulhoque99@gmail.com>
2024-09-19 15:29:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // This will ignore ESLint errors during the build process
+  },
 };
 
 export default nextConfig;
