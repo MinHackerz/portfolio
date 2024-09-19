466a84378b78255755f1e6af74e607c52639bea9
Menajul Hoque <menajulhoque99@gmail.com>
2024-09-19 20:55:15 +0530

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-// next-sitemap.config.js
-
-module.exports = {
-  siteUrl: 'https://menajul.vercel.app',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: 'daily',
-  priority: 0.7,
-};
