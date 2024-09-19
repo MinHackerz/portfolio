db016925493bbd9e3737822e34109a10ce06e135
Menajul Hoque <menajulhoque99@gmail.com>
2024-09-19 20:30:19 +0530

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+module.exports = {
+  siteUrl: 'https://menajul.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
